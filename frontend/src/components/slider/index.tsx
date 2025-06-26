56aba27a1f08a29f95724a6a03e0e9ebc9c0c7e6
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-06-26 19:55:16 +0530

    feat: implement Slider component for image comparison with drag functionality; update Footer styles for better visibility

---
+++ frontend/src/components/slider/index.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export const Slider = ({ image1, image2 }: { image1: string, image2: string }) => {
+    const [sliderPosition, setSliderPosition] = useState(50);
+    const [isDragging, setIsDragging] = useState(false);
+
+    const handleMove = (clientX: number, rect: DOMRect) => {
+        const x = Math.max(0, Math.min(clientX - rect.left, rect.width));
+        const percent = Math.max(0, Math.min((x / rect.width) * 100, 100));
+        setSliderPosition(percent);
+    };
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (!isDragging) return;
+        const rect = event.currentTarget.getBoundingClientRect();
+        handleMove(event.clientX, rect);
+    };
+
+    const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+        if (!isDragging) return;
+        const rect = event.currentTarget.getBoundingClientRect();
+        if (event.touches.length > 0) {
+            const touch = event.touches[0];
+            handleMove(touch.clientX, rect);
+        }
+    };
+
+    const handleInteractionStart = () => setIsDragging(true);
+    const handleInteractionEnd = () => setIsDragging(false);
+
+    return (
+        <div className="w-full relative" onMouseUp={handleInteractionEnd} onTouchEnd={handleInteractionEnd}>
+            <div
+                className="relative w-full max-w-[700px] aspect-[80/45] m-auto overflow-hidden select-none"
+                onMouseMove={handleMouseMove}
+                onTouchMove={handleTouchMove}
+                onMouseDown={handleInteractionStart}
+                onTouchStart={handleInteractionStart}
+            >
+                <Image
+                    alt="aligner360"
+                    fill
+                    priority
+                    src={image1 ?? 'https://images.unsplash.com/photo-1523435324848-a7a613898152?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80'}
+                />
+
+                <div
+                    className="absolute top-0 left-0 right-0 w-full max-w-[700px] aspect-[70/45] m-auto overflow-hidden select-none"
+                    style={{ clipPath: `inset(0 ${100 - sliderPosition}% 0 0)` }}
+                >
+                    <Image
+                        fill
+                        priority
+                        alt="aligner360"
+                        src={image2 ?? "https://images.unsplash.com/photo-1598875791852-8bb153e713f0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2010&q=80"}
+                    />
+                </div>
+                <div
+                    className="absolute top-0 bottom-0 w-1 bg-white cursor-ew-resize"
+                    style={{
+                        left: `calc(${sliderPosition}% - 1px)`,
+                    }}
+                >
+                    <div className="bg-white absolute rounded-full h-3 w-3 -left-1 top-[calc(50%-5px)]" />
+                </div>
+            </div>
+        </div>
+    );
+};
